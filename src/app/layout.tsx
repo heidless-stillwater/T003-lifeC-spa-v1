5b027536562f341c164e13a2443630864f7297b0
rob <heidlessemail14@gmail.com>
2025-07-30 17:05:41 +0000

    Primary Headline Font 	Font Name: Lato 	Recommended Weights: Bold (700)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Belleza&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Lato:wght@700;800&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
           <ThemeProvider
